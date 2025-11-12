6e1ff8b15b6fb918cac3cfe37d9158751671720a
ahmedyraw <ahmedyraw@gmail.com>
2025-11-13 00:51:23 +0300

    Initial project setup

---
+++ src/main/java/com/example/unihub/UnihubApplication.java
@@ -0,0 +1,13 @@
+package com.example.unihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnihubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnihubApplication.class, args);
+	}
+
+}
